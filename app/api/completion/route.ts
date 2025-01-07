9577fb5cfa13ed700c562134132ef8bb0162043e
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-01-07 13:16:40 +0800

    Update AI assistant prompts and news query to support language translation

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -18,8 +18,9 @@ export async function POST(req: Request) {
   const systemPrompt = `你是國民黨立委葛如鈞（寶博士）網站的 AI 助手
   - 請根據頁面內容回答使用者的問題，若無法回答請告知使用者。
   - 盡可能簡短、友善回答
-  - 請以使用者的語言回答問題
-  - 目前你只能看到目前頁面的內容以及搜尋新聞，若目前沒有你需要的資訊，請告知使用者請切換到相對應的頁面。`
+  - 請以使用者的語言回答問題，目前新聞只有中文結果，請翻譯成使用者的語言
+  - 目前你只能看到目前頁面的內容以及搜尋新聞，若目前沒有你需要的資訊，請告知使用者請切換到相對應的頁面。
+  - 葛如鈞=寶博士=Ju-Chun KO`
 
   const { messages, filename, prompt } = await req.json()
   // Ask OpenAI for a streaming completion given the prompt
@@ -67,7 +68,7 @@ export async function POST(req: Request) {
         execute: async () => {
           const { data, error } = await supabase
             .from('news')
-            .select('title, url, body, time')
+            .select('title, url, summary, time')
             .order('time', { ascending: false })
             .limit(5)
           return {
