941a9242a57a3dd9ccac6994550d7dfbcf741269
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-03-03 16:33:15 +0800

    feat: enhance completion API with semantic site search and increase max tokens

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -17,19 +17,23 @@ const embeddingModel = openai.embedding('text-embedding-3-small')
 
 export async function POST(req: Request) {
   const systemPrompt = `你是國民黨立委葛如鈞（寶博士）網站的 AI 助手
-  - 請根據頁面內容回答使用者的問題，若無法回答請告知使用者。
   - 盡可能簡短、友善回答
+  - 盡可能使用工具來提供使用者盡可能準確與完整的資訊
   - 請以使用者的語言回答問題，目前新聞只有中文結果，若使用者不是用中文進行提問，請翻譯成使用者的語言
-  - 若目前沒有你需要的資訊，請嘗試使用搜尋新聞或檢視頁面功能
   - 新聞來源有多個，會出現重複新聞，請自行總結後再和使用者說，並附上所有網址和來源名稱，像這樣 [自由時報](https://xxx) [中央社](https://xxx)
-  - 葛如鈞=寶博士=Ju-Chun KO`
+  - 葛如鈞=寶博士=Ju-Chun KO
+
+  <semanticSiteSearch>
+  semanticSiteSearch 會提供 path 'pages/docs/manual/hobby.zh-TW.mdx' 你可以轉換成對應的官網連結提供給使用者 'https://juchunko.com/docs/manual/hobby'
+  </semanticSiteSearch>
+  `
 
   const { messages, filename, prompt } = await req.json()
   // Ask OpenAI for a streaming completion given the prompt
   const result = streamText({
     model: openai('gpt-4o-mini'),
     temperature: 0.6,
-    maxTokens: 4096,
+    maxTokens: 8_192,
     messages: [
       { role: 'system', content: systemPrompt },
       ...messages,
@@ -38,7 +42,7 @@ export async function POST(req: Request) {
         content: prompt,
       },
     ],
-    maxSteps: 5,
+    maxSteps: 8,
     experimental_transform: smoothStream({
       delayInMs: 80,
       chunking: 'word',
@@ -78,7 +82,7 @@ export async function POST(req: Request) {
               }))
         },
       }),
-      searchWebsite: tool({
+      semanticSiteSearch: tool({
         description: 'Search this website using semantic search',
         parameters: z.object({
           keyword: z.string().describe('the keyword to search for'),
@@ -91,19 +95,29 @@ export async function POST(req: Request) {
           })
           // embed search keyword
           const { data, error } = await supabase
-            .rpc('official_website', {
+            .rpc('match_official_website', {
               query_embedding: embedding,
               match_threshold: 0.4,
             })
-            .eq('language', language)
-            .select('title, paragraph')
-            .limit(10)
-
+            .select('path')
+            .limit(20)
+          if (!data?.length) return []
+          // list all items in same path
+          const result: any[] = []
+          for (let path of [...new Set(data.map((item) => item.path))]) {
+            const { data: samePathData, error: samePathError } = await supabase
+              .from('official_website')
+              .select('title, paragraph, language, path')
+              .eq('path', path)
+              .eq('language', language)
+              .limit(20)
+            if (samePathData?.length) result.push(...samePathData)
+          }
           if (error) {
             console.error(error)
             return 'An error occurred while searching for news articles'
           }
-          return data
+          return result
         },
       }),
       latestNews: tool({
