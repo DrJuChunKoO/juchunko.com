b1f7a961eff54a5faf14f6ee1115a422c621aae2
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-06-23 16:37:15 +0800

    Migrate to Cloudflare Worker

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import { SkipNavLink } from 'nextra-theme-docs'
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="preconnect" href="https://rsms.me/" />
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-          <script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.18/build/spline-viewer.js" />
-        </Head>
-        <body>
-          <SkipNavLink styled />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
-}
 
-export default MyDocument
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.18/build/spline-viewer.js" />
+      </Head>
+      <body>
+        <SkipNavLink styled />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
